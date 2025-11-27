571d2a4491493f6a4d6f91ecb52d0e60ce8a7a18
pentestpop <162646448+pentestpop@users.noreply.github.com>
2025-11-27 14:17:13 -0600

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,1 +1,8 @@
+// preload.js
+const { contextBridge, ipcRenderer } = require('electron');
 
+contextBridge.exposeInMainWorld('api', {
+  runScript: (script) => ipcRenderer.invoke('run-applescript', script),
+  selectFolder: () => ipcRenderer.invoke('select-folder'),
+  downloadVideo: (options) => ipcRenderer.invoke('download-video', options)
+});
